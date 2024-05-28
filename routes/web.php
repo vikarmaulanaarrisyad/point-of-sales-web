cda4e7a7f1d9ebb72e89b806ea0a815da23fc88b
Vikar Maulana Arrisyad <vikar.maulana.arrisyad@gmail.com>
2024-05-28 16:45:41 +0700

    remove profile.show web route

--- routes/web.php
+++ routes/web.php
@@ -38,7 +38,7 @@ Route::group(['middleware' => ['auth']], function () {
             Route::put('/users/{users}/update', 'update')->name('users.update');
             Route::post('/users', 'store')->name('users.store');
             Route::delete('/users/{users}/destroy', 'destroy')->name('users.destroy');
-            Route::delete('/user/profile', 'show')->name('profile.show');;
+            // Route::delete('/user/profile', 'show')->name('profile.show');
         });
     });
 
