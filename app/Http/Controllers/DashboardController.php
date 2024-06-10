8373bf00549cf6ec70bb580706aa098e3b34f5f4
Vikar Maulana Arrisyad <vikar.maulana.arrisyad@gmail.com>
2024-06-10 23:00:54 +0700

    fix dashboard

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -12,7 +12,7 @@ use Illuminate\Http\Request;
 
 class DashboardController extends Controller
 {
-    public function index1()
+    public function index()
     {
         // Set timezone and locale
         Carbon::setLocale('id');
@@ -93,84 +93,6 @@ class DashboardController extends Controller
             'targetPenjualanPerBulan',
             'dailyProfits',
             'dailyProfit',
-            'totalSalesPerDay',
-            'monthlyProfits',
-            'totalSalesPerMonth',
-            'totalSalesToday'
-        ));
-    }
-    public function index()
-    {
-        // Set timezone and locale
-        Carbon::setLocale('id');
-        $carbon = new Carbon();
-        $carbon->setTimezone('Asia/Jakarta');
-
-        // Fetch counts
-        $totalProduk = Product::count();
-        $totalKategori = Category::count();
-        $totalPelanggan = Pelanggan::count();
-        $totalKaryawan = Karyawan::count();
-
-        // Fetch latest products and products with low stock
-        $produkTerbaru = Product::latest()->take(10)->get();
-        $produkKurangStok = Product::where('stok', '<', 10)->get();
-
-        // Fetch yearly sales targets for employees
-        $tahun = date('Y');
-        $karyawan = new Karyawan();
-        $targetPenjualanPerBulan = $karyawan->hitungTargetPenjualanPerBulan($tahun);
-
-        // Calculate daily profit and total sales per day
-        $dailyProfits = [];
-        $totalSalesPerDay = [];
-        $startDate = $carbon->copy()->subDays(30)->startOfDay();
-        $endDate = $carbon->copy()->endOfDay();
-
-        $penjualans = Penjualan::whereBetween('created_at', [$startDate, $endDate])->get();
-        $avgHargaBeli = Product::avg('harga_beli');
-
-        foreach ($penjualans->groupBy(function ($date) {
-            return Carbon::parse($date->created_at)->format('Y-m-d');
-        }) as $date => $penjualansForDate) {
-            $totalSales = $penjualansForDate->sum('total_harga');
-            $totalItems = $penjualansForDate->sum('total_item');
-            $totalCost = $totalItems * $avgHargaBeli;
-            $dailyProfit = $totalSales - $totalCost;
-
-            $dailyProfits[$date] = $dailyProfit;
-            $totalSalesPerDay[$date] = $totalSales;
-        }
-
-        // Calculate monthly profit and total sales per month
-        $monthlyProfits = [];
-        $totalSalesPerMonth = [];
-        $currentYear = $carbon->copy()->format('Y');
-        $penjualans = Penjualan::whereYear('created_at', $currentYear)->get();
-
-        foreach ($penjualans->groupBy(function ($date) {
-            return Carbon::parse($date->created_at)->format('m');
-        }) as $month => $penjualansForMonth) {
-            $totalSales = $penjualansForMonth->sum('total_harga');
-            $totalItems = $penjualansForMonth->sum('total_item');
-            $totalCost = $totalItems * $avgHargaBeli;
-            $monthlyProfit = $totalSales - $totalCost;
-
-            $monthlyProfits[$month] = round($monthlyProfit);
-            $totalSalesPerMonth[$month] = $totalSales;
-        }
-
-        $totalSalesToday = Penjualan::whereDate('created_at', $endDate)->sum('total_harga');
-
-        return view('dashboard.index', compact(
-            'totalProduk',
-            'totalKategori',
-            'totalPelanggan',
-            'totalKaryawan',
-            'produkTerbaru',
-            'produkKurangStok',
-            'targetPenjualanPerBulan',
-            'dailyProfits',
             'totalSalesPerDay',
             'monthlyProfits',
             'totalSalesPerMonth',
--- resources/views/dashboard/small_box.blade.php
+++ resources/views/dashboard/small_box.blade.php
@@ -1,99 +1,95 @@
- <div class="row">
-     @can('Total Produk')
-         <div class="col-lg-3 col-6">
+<div class="row">
+    @can('Total Produk')
+        <div class="col-lg-3 col-6">
+            <div class="small-box bg-info">
+                <div class="inner">
+                    <h3>{{ $totalProduk }}</h3>
+                    <p>Total Produk</p>
+                </div>
+                <div class="icon">
+                    <i class="fas fa-cubes"></i>
+                </div>
+                <a href="{{ route('product.index') }}" class="small-box-footer">More info <i
+                        class="fas fa-arrow-circle-right"></i></a>
+            </div>
+        </div>
+    @endcan
 
-             <div class="small-box bg-info">
-                 <div class="inner">
-                     <h3>{{ $totalProduk }}</h3>
-                     <p>Total Produk</p>
-                 </div>
-                 <div class="icon">
-                     <i class="fas fa-cubes"></i>
-                 </div>
-                 <a href="{{ route('product.index') }}" class="small-box-footer">More info <i
-                         class="fas fa-arrow-circle-right"></i></a>
-             </div>
-         </div>
-     @endcan
+    @can('Total Kategori')
+        <div class="col-lg-3 col-6">
+            <div class="small-box bg-success">
+                <div class="inner">
+                    <h3>{{ $totalKategori }}</h3>
+                    <p>Total Kategori</p>
+                </div>
+                <div class="icon">
+                    <i class="fas fa-cube"></i>
+                </div>
+                <a href="{{ route('category.index') }}" class="small-box-footer">More info <i
+                        class="fas fa-arrow-circle-right"></i></a>
+            </div>
+        </div>
+    @endcan
 
-     @can('Total Kategori')
-         <div class="col-lg-3 col-6">
+    @can('Total Pelanggan')
+        <div class="col-lg-3 col-6">
+            <div class="small-box bg-warning">
+                <div class="inner">
+                    <h3>{{ $totalPelanggan }}</h3>
+                    <p>Total Pelanggan</p>
+                </div>
+                <div class="icon">
+                    <i class="fas fa-users"></i>
+                </div>
+                <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+            </div>
+        </div>
+    @endcan
 
-             <div class="small-box bg-success">
-                 <div class="inner">
-                     <h3>{{ $totalKategori }}</h3>
-                     <p>Total Kategori</p>
-                 </div>
-                 <div class="icon">
-                     <i class="fas fa-cube"></i>
-                 </div>
-                 <a href="{{ route('category.index') }}" class="small-box-footer">More info <i
-                         class="fas fa-arrow-circle-right"></i></a>
-             </div>
-         </div>
-     @endcan
-
-     @can('Total Pelanggan')
-         <div class="col-lg-3 col-6">
-             <div class="small-box bg-warning">
-                 <div class="inner">
-                     <h3>{{ $totalPelanggan }}</h3>
-                     <p>Total Pelanggan</p>
-                 </div>
-                 <div class="icon">
-                     <i class="fas fa-users"></i>
-                 </div>
-                 <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
-             </div>
-         </div>
-     @endcan
-
-     @can('Total Karyawan')
-         <div class="col-lg-3 col-6">
-
-             <div class="small-box bg-danger">
-                 <div class="inner">
-                     <h3>{{ $totalKaryawan }}</h3>
-                     <p>Total Karyawan</p>
-                 </div>
-                 <div class="icon">
-                     <i class="fas fa-user-friends"></i>
-                 </div>
-                 <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
-             </div>
-         </div>
-     @endcan
- </div>
+    @can('Total Karyawan')
+        <div class="col-lg-3 col-6">
+            <div class="small-box bg-danger">
+                <div class="inner">
+                    <h3>{{ $totalKaryawan }}</h3>
+                    <p>Total Karyawan</p>
+                </div>
+                <div class="icon">
+                    <i class="fas fa-user-friends"></i>
+                </div>
+                <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+            </div>
+        </div>
+    @endcan
+</div>
 
 
- <div class="row">
-     @can('Total Penjualan Harian')
-         <div class="col-lg-6 col-6">
-             <div class="small-box bg-primary">
-                 <div class="inner">
-                     <h3>{{ format_uang($totalSalesToday) }}</h3> <!-- Contoh akses nilai untuk tanggal tertentu -->
-                     <p>Total Penjualan Harian</p>
-                 </div>
-                 <div class="icon">
-                     <i class="fas fa-shopping-cart"></i>
-                 </div>
-                 <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
-             </div>
-         </div>
-     @endcan
-     @can('Total Laba Harian')
-         <div class="col-lg-6 col-6">
-             <div class="small-box bg-secondary">
-                 <div class="inner">
-                     {{--  <h3>{{ format_uang($dailyProfit) }}</h3> <!-- Sesuaikan dengan variabel yang menyimpan laba perhari -->  --}}
-                     <h3>{{ format_uang($dailyProfit ?? 0) }}</h3>
-                     <p>Total Laba Harian</p>
-                 </div>
-                 <div class="icon">
-                     <i class="fas fa-dollar-sign"></i>
-                 </div>
-                 <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
-             </div>
-         </div>
-     @endcan
- </div>
+<div class="row">
+    @can('Total Penjualan Harian')
+        <div class="col-lg-6 col-6">
+            <div class="small-box bg-primary">
+                <div class="inner">
+                    <h3>{{ format_uang($totalSalesToday) }}</h3>
+                    <p>Total Penjualan Harian</p>
+                </div>
+                <div class="icon">
+                    <i class="fas fa-shopping-cart"></i>
+                </div>
+                <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+            </div>
+        </div>
+    @endcan
+    @can('Total Laba Harian')
+        <div class="col-lg-6 col-6">
+            <div class="small-box bg-secondary">
+                <div class="inner">
+                    <h3>{{ format_uang($dailyProfit ?? 0) }}</h3>
+                    <p>Total Laba Harian</p>
+                </div>
+                <div class="icon">
+                    <i class="fas fa-dollar-sign"></i>
+                </div>
+                <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+            </div>
+        </div>
+    @endcan
+</div>
